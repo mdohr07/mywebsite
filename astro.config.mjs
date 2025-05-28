1a3c0be0adf59bbbb9ff1d16ee4ffe9840fb7780
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2025-05-28 09:02:09 +0200

    fix bugs, simplify astro.config, move markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    output: 'static',
-    trailingSlash: 'always',
-    site: 'https://devidev.io',
-    prefetch: false,
-
-    integrations: [
-        tailwind(),
-        sitemap(),
-        compress({
-            CSS: true,
-            SVG: false,
-            Image: false,
-            HTML: {
-                "html-minifier-terser": {
-                    collapseWhitespace: true,
-                    // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
-                    minifyCSS: true,
-                    minifyJS: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    // removeEmptyElements: true, // It removes sometimes SVGs
-                    removeRedundantAttributes: true
-                },
-            },
-            JavaScript: {
-                'terser': {
-                    compress: {
-                        drop_console: true,
-                        drop_debugger: true,
-                    }
-                }
-            },        
-        })
-    ]
+  integrations: [tailwind(), compress(), sitemap()],
+  output: 'static',
+  trailingSlash: 'always',
+  site: 'https://mdohr.space',
+  prefetch: false,
 });
